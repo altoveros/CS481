f1f46e82c4515755a4801e6038a1f974ba18d24a
Ian Altoveros <altoverosi@gmail.com>
2021-10-04 19:34:17 -0700

    Sample Android Program

---
+++ cs481_hw1/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+    void main() => runApp(MyApp());
+
+
+  class MyApp extends StatelessWidget{
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        title: 'Welcome to Flutter',
+        home:Scaffold(
+          appBar: AppBar(
+            title: Text('Welcome to Flutter'),
+          ),
+          body: Center(
+            child: Text('''
+            Hello World
+            Ian Altoveros
+            Spring 2021
+            Kachow!
+            '''),
+          ),
+        ),
+      );
+  }
+  }
